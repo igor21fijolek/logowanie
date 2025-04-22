30e41134a4ce23d1a565b5ccf96f3f7723db48e4
igor21fijolek <fijolekigor7@gmail.com>
2025-04-22 21:56:21 +0200

    nie wiem
    nie wiem

--- ćw/client/app.js
+++ ćw/client/app.js
@@ -3,6 +3,8 @@ async function rejestracja() {
     let haslo =  document.getElementById('haslo').value
     let haslo2 =  document.getElementById('haslo2').value
 
+    console.log(`Login: ${login}, Hasło: ${haslo}, Hasło2: ${haslo2}`)
+
     let url = `http://localhost:3000/rejestracja/${login}/${haslo}`
     if(haslo === haslo2){
         let res = await fetch(url)
@@ -15,5 +17,23 @@ async function rejestracja() {
         }
     }else{
         document.getElementById('res-info').innerHTML = 'hasla nie sa takie same'
+    }
+}
+async function logowanie(){
+    let login  = document.getElementById('login').value
+    let haslo  =document.getElementById('haslo').value
+    console.log(`Login: ${login}, Hasło: ${haslo}`)
+
+
+    let url = `http://localhost:3000/logowanie/${login}/${haslo}"`
+    let res = await fetch(url)
+    let data = await res.json()
+    console.log(data);
+
+    if( data.length === 0){
+        document.getElementById('login-info').innerHTML = 'nie zalogowano'
+    }else{
+        document.getElementById('login-info').innerHTML = 'zalogowano'
+
     }
 }
